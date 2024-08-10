459479b868bcd07b56b3ef2b919fb366dd443f64
Brandon Nguyen <nguyen.brandon316@gmail.com>
2024-08-10 21:24:00 +0000

    Finished basic script for repository interface

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,37 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import com.example.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/*
+ * Point of the repository class is that it acts as a way to write custom queries in a JpaRepository using JPQL
+ * It abstacts away from boilerplate logic by directly mapping queries to ORM (Object Relational Mapper) entities
+ * It is also vendor-agnostic: JPQL query does not change between, like MySQL 
+ * Sort of treat this like the DAO class we did back in project 1, however only implement queries that are otherwise
+ * not in the defined methods
+ 
+    Use table named: account
+    account_id (primary key) : type int
+    username (unique) : type varchar(255)
+    password: type varchar(255)
+
+    check if account exists (select)
+    -------------------------------------------------------------------
+    login into account with password (select) 
+*/
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    //Checks to see if the account exists with username
+    @Query("Select a from Account a where a.username = :username")
+    Account checkAccount(@Param("username") String username);
+
+    //checks to see if account username exists and matches with password
+    @Query("Select a from Account a where a.username = :username and a.password = :password")
+    Account login(@Param("username") String username, @Param("password") String password);
+
+    // Account findAccountByUsername(String username);
+    // Account findAccountByUsernameAndPassword(String username, String password)
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,33 @@
 package com.example.repository;
 
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/*
+ * Point of the repository class is that it acts as a way to write custom queries in a JpaRepository using JPQL
+ * Sort of treat this like the DAO class we did back in project 1, however only implement queries that are otherwise
+ * not in the defined methods
+ * It abstacts away from boilerplate logic by directly mapping queries to ORM (Object Relational Mapper) entities
+ * It is also vendor-agnostic: JPQL query does not change between, like MySQL 
+ 
+    Use table named: message
+    message_id (primary key) : type int
+    posted_by (foregin key) : type int
+    message_text : type varchar(255)
+    time_posted_epoch : type long
+
+    retrieve all messages from a user_id (select)
+*/
+
 public interface MessageRepository {
+    //to see all messages from a specific user with their id
+    List<Message> findMessagesByPostedBy(Long postedBy); 
+
+    // @Query("Select m from Message m where m.postedBy = :postedBy")
+    // List<Message> getMessagesFromUser(@Param("postedBy") Long postedBy);
 }
